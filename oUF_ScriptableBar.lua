c334a0a7050225b7dd17e146cc4d1b471d8b0bfb
Scott Sibley <sisibley@gmail.com>
2011-08-06 01:20:18 -0500

    Fixes. Texture, min, max, value, orientation. Should all update every tick.

--- oUF_ScriptableBar.lua
+++ oUF_ScriptableBar.lua
@@ -6,37 +6,41 @@ oUF.ALIGN_LEFT, oUF.ALIGN_CENTER, oUF.ALIGN_RIGHT, oUF.ALIGN_MARQUEE, oUF.ALIGN_
 oUF.SCROLL_RIGHT, oUF.SCROLL_LEFT = 1, 2
 
 local Update = function(self)
-	local widget = self.widget
-	if widget and widget:GetObjectType() == "StatusBar" then
-		widget:SetValue(widget.val1 * 100)
+	local widget = self
+	local bar = widget.bar
+	if bar and bar:GetObjectType() == "StatusBar" then
+		bar:SetMinMaxValues(widget.min, widget.max)
+		bar:SetStatusBarTexture(widget.texture)
+		bar:SetValue(widget.val1 * widget.max)
+		bar:SetHeight(widget.height)
+		bar:SetWidth(widget.length)
 		local r, g, b, a = 1, 1, 1, 1
 		
 		if widget.color1 and widget.widget1 then
-			r, g, b, a, = widget.color1.ret1, widget.color1.ret2, widget.color1.ret3, widget.color1.ret4
+			r, g, b, a = widget.color1.ret1, widget.color1.ret2, widget.color1.ret3, widget.color1.ret4
 		elseif widget.color2 and widget.widget2 then
-			r, g, b, a, = widget.color2.ret1, widget.color2.ret2, widget.color2.ret3, widget.color2.ret4
+			r, g, b, a = widget.color2.ret1, widget.color2.ret2, widget.color2.ret3, widget.color2.ret4
 		end
 		if type(r) == "number" then
-			widget:SetStatusBarColor(r, g, b, a)
+			bar:SetStatusBarColor(r, g, b, a)
 		end
 	end
+print("hmmm")
 end
 
 local Enable = function(self, unit)
 	local bar = self.ScriptableBar
-	if bar and text:GetObjectType() == "StatusBar" then
+	if bar and bar:GetObjectType() == "StatusBar" then
 		assert(self.core)
 		local col, row, layer = 0, 0, 0
 		local errorLevel = 2
 		local name = bar.name or "ScriptableBar"
 
-		local widget = bar.widget or WidgetBar:New(self.core, name, text, row, col, layer, errorLevel, Update)
-		if widget.configModified then widget:Init() end
-
+		local widget = bar.widget or WidgetBar:New(self.core, name, bar, row, col, layer, errorLevel, Update)
 		widget.environment.unit = unit
+		widget:Start()
+		widget.bar = bar
 		bar.widget = widget
-		bar.widget:Start()
-		bar.widget.bar = text
 	end
 	return true
 end
